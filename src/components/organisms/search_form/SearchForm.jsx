7e2310f6d318ee512d5b8147d73c490d4e0ca9d7
liviaab <livia.almeida@appprova.com.br>
2019-10-20 23:10:30 -0300

    Dinamic search form and its tests

--- src/components/organisms/search_form/SearchForm.jsx
+++ src/components/organisms/search_form/SearchForm.jsx
@@ -1,31 +1,80 @@
-import React from 'react'
-import { Combobox, TextInput, Button } from 'evergreen-ui'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Combobox, TextInput, Button, toaster} from 'evergreen-ui'
 import style from './SearchForm.module.scss'
+import { courtsService, processesService } from '../../../services'
 
-const SearchForm = () => (
-	<div className={style.searchForm}>
-		<Combobox 
-			placeholder='Tribunal'
-			className={style.courtField}
-			width="100%"
-			height={40}
-		/>
-		<TextInput
-			name='input-process-number'
-			placeholder='Número do processo'
-			className={style.processField}
-			height={40}
-		/>
-		<Button 
-			iconBefore="search" 
-			className={style.submit}
-			background-color="#9a9a9a"	
-			marginLeft={20}
-			height={40}
-		>
-			Buscar
-		</Button>
-	</div>
-)
+class SearchForm extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			courts: [],
+			court_name: '',
+			process_number: '',
+		}
+	}
+
+	componentDidMount() {
+		courtsService().get().then(response => {
+			const courts = response.data.map(item => ({'id': item.id, 'label': item.initials}))
+			this.setState({ courts })
+		}).catch(error => toaster.danger('Algo de errado aconteceu durante' +
+																			'a busca de tribunais.', 500))
+	}
+
+	updateField = (field, value) =>	this.setState({ [field]: value })
+
+	handleButton = (event) => {
+		const { process_number, court_name } = this.state
+
+		processesService().get(process_number).then(response => {
+			this.props.updateParent({ ...response.data, court_name })
+		}).catch(error => toaster.danger('Algo de errado aconteceu durante' +
+																			'a busca do processo.', 500))
+	}
+
+	render(){
+		const { courts, process_number } = this.state
+
+		return(
+			<div className={style.searchForm}>
+				<Combobox
+					items={courts}
+					itemToString={item => item ? item.label : ''}
+					placeholder='Tribunal'
+					className={style.courtField}
+					width="100%"
+					height={40}
+					onChange={e => this.updateField('court_name', e.label)}
+				/>
+				<TextInput
+					id='input-process-number'
+					name='input-process-number'
+					placeholder='Número do processo'
+					className={style.processField}
+					height={40}
+					onChange={e => this.updateField('process_number', e.target.value)}
+					value={process_number}
+				/>
+				<Button
+					id="submit"
+					iconBefore="search"
+					className={style.submit}
+					background-color="#9a9a9a"
+					marginLeft={20}
+					height={40}
+					onClick={this.handleButton}
+				>
+					Buscar
+				</Button>
+			</div>
+		)
+	}
+}
+
+SearchForm.propTypes = {
+	updateParent: PropTypes.func.isRequired
+}
 
 export default SearchForm
---
+++ src/components/organisms/search_form/SearchForm.test.js
@@ -0,0 +1,113 @@
+import nock from 'nock'
+import React from 'react'
+import { shallow } from 'enzyme'
+import SearchForm from './SearchForm'
+import courtsResponse from '../../../fixtures/courts'
+import processResponse from '../../../fixtures/process'
+
+const initCourtNock = () => (
+  nock(process.env.REACT_APP_BASE_URL)
+    .get('/courts')
+    .reply(200, courtsResponse, { 'Content-Type': 'application/json' })
+)
+
+describe('SearchForm organism', () => {
+  const processNumber = '0067154-55.2010.8.02.0001'
+  let mockFunction
+  let searchFormWrapper
+
+  beforeEach(() => {
+    process.env.REACT_APP_BASE_URL = 'http://base.url/api/v1'
+    mockFunction = jest.fn()
+  })
+
+  it('loads all courts after mount', (done) => {
+    initCourtNock()
+    searchFormWrapper = shallow(<SearchForm updateParent={mockFunction} />)
+    setTimeout(() => {
+      done()
+      expect(searchFormWrapper.state().courts).toHaveLength(courtsResponse.length)
+    }, 200)
+  })
+
+  it('does not update state if api does not return courts response', (done) => {
+    nock(process.env.REACT_APP_BASE_URL)
+      .get('/courts')
+      .reply(500, 'Internal Server Error', { 'Content-Type': 'application/json' })
+
+
+    searchFormWrapper = shallow(<SearchForm updateParent={mockFunction} />)
+    setTimeout(() => {
+      done()
+      expect(searchFormWrapper.state().courts).toHaveLength(0)
+    }, 200)
+  })
+
+  it('calls the `updateParent` function after search for a valid process number', (done) => {
+    const initialState = {
+      courts: [],
+      court_name: '',
+      process_number: ''
+    }
+    const expectedFinalState = {
+      courts: courtsResponse.map(item => ({'id': item.id, 'label': item.initials})),
+      court_name: '',
+      process_number: processNumber
+    }
+
+    initCourtNock()
+    nock(process.env.REACT_APP_BASE_URL)
+      .get(`/processes/${processNumber}`)
+      .reply(200, processResponse, { 'Content-Type': 'application/json' })
+
+
+    searchFormWrapper = shallow(<SearchForm updateParent={mockFunction} />)
+    expect(searchFormWrapper.state()).toEqual(initialState)
+
+    searchFormWrapper.setState({ process_number: processNumber })
+    searchFormWrapper.find('#submit').simulate('click')
+
+    setTimeout(() => {
+      done()
+      expect(mockFunction).toHaveBeenCalledTimes(1)
+      expect(searchFormWrapper.state()).toEqual(expectedFinalState)
+    }, 200)
+  })
+
+  it('does not call `updateParent` function  if the process number is invalid', (done) => {
+    initCourtNock()
+    nock(process.env.REACT_APP_BASE_URL)
+      .get(`/processes/undefined`)
+      .reply(500, 'Internal Server Error', { 'Content-Type': 'application/json' })
+
+    searchFormWrapper = shallow(<SearchForm updateParent={mockFunction} />)
+    setTimeout(() => {
+      done()
+      expect(mockFunction).not.toHaveBeenCalled()
+    }, 200)
+  })
+
+  it('updates state if court is selected', () => {
+    initCourtNock()
+    const event = {id: 1, label: 'TJAL'}
+
+    searchFormWrapper = shallow(<SearchForm updateParent={mockFunction} />)
+    const combobox = searchFormWrapper.find('Combobox')
+    combobox.simulate('change', event)
+    expect(searchFormWrapper.state().court_name).toEqual('TJAL')
+  })
+
+  it('updates state if process number is selected', () => {
+    initCourtNock()
+    const event = {
+        target: {
+          value: processNumber
+        }
+      }
+
+    searchFormWrapper = shallow(<SearchForm updateParent={mockFunction} />)
+    const input = searchFormWrapper.find('#input-process-number')
+    input.simulate('change', event)
+    expect(searchFormWrapper.state().process_number).toEqual(processNumber)
+  })
+})
