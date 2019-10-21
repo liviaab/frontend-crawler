cc6dc86b4dbd9ffd1bcb48889bd68690c2e961ef
liviaab <livia.almeida@appprova.com.br>
2019-10-21 01:17:42 -0300

    Dinamic ListOfChanges organism, tests

--- src/components/organisms/list_of_changes/ListOfChanges.jsx
+++ src/components/organisms/list_of_changes/ListOfChanges.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { Table } from 'evergreen-ui'
+import PropTypes from 'prop-types'
 import style from './ListOfChanges.module.scss'
 
-const buildBody = () => (
-  <Table.Body height={240}>
-      <Table.Row key="1" height="auto" paddingY={12} className={style.change}>
-        <Table.TextCell>21/09/2019</Table.TextCell>
-        <Table.TextCell>Lorem ipsum dolor sit amet</Table.TextCell>
-      </Table.Row>
-      <Table.Row key="2" height="auto" paddingY={12} className={style.change}>
-        <Table.TextCell>21/09/2019</Table.TextCell>
-        <Table.TextCell className={style.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Table.TextCell>
-      </Table.Row>
-  </Table.Body>
-)
+const buildBody = (changes) => {
+  if(changes == []) {
+    return <Fragment />
+  }
+
+  return (
+    <Table.Body>
+      {changes.map(change => (
+            <Table.Row key={change.id} height="auto" paddingY={12} className={style.change}>
+              <Table.TextCell isNumber >{change.date}</Table.TextCell>
+              <Table.TextCell className={style.description}>{change.description}</Table.TextCell>
+            </Table.Row>)
+        )
+      }
+    </Table.Body>
+  )
+}
 
 const buildHeader = () => (
   <Table.Head>
@@ -30,15 +29,20 @@ const buildHeader = () => (
   </Table.Head>
 )
 
-const buildTable = () => {
+const buildTable = (changes) => {
   return(
     <Table className={style.changesTable}>
       {buildHeader()}
-      {buildBody()}
+      {buildBody(changes)}
     </Table>
   )
 }
 
-const ListOfChanges = () => buildTable()
+const ListOfChanges = ({ changes }) => buildTable(changes)
+
+ListOfChanges.propTypes = {
+  changes: PropTypes.array.isRequired
+}
+
 
 export default ListOfChanges
